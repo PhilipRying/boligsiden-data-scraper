4f818b4d69fc56b4fe6985c537976f66dab06ad5
Philip Rying <philiprying@gmail.com>
2020-04-11 21:27:35 +0200

    Scraped data added to csv

--- scrape_site.py
+++ scrape_site.py
@@ -1,6 +1,27 @@
 import time
 import pandas as pd
+import csv
+
+import sqlite3
+from sqlite3 import Error
+
+
+
+
 
 def scrape_page(driver):
     adress = driver.find_element_by_class_name('title').get_attribute('innerText')
-    print(adress)
+    area = driver.find_element_by_xpath('/html/body/div[4]/div/div[3]/div/div/div[2]/div/table/tbody/tr[2]/td[2]').get_attribute('innerText')
+    sold_price = driver.find_element_by_xpath('/html/body/div[4]/div/div[3]/div/div/div[1]/div/table/tbody/tr[8]/td[2]').get_attribute('innerText')
+    sold_date = driver.find_element_by_xpath('/html/body/div[4]/div/div[3]/div/div/div[1]/div/table/tbody/tr[8]/td[1]').get_attribute('innerText')
+    build_year = driver.find_element_by_xpath('/html/body/div[4]/div/div[3]/div/div/div[2]/div/table/tbody/tr[7]/td[2]').get_attribute('innerText') 
+
+    with open('data.csv','a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([adress,area,sold_price,sold_date, build_year])
+
+
+        
+    
+
+
