912116904aae5da24f0dbff92ea20695b0f0795c
Philip Rying <philiprying@gmail.com>
2020-04-11 15:10:34 +0200

    FInished site navigation
    navigate:site can now be called with postal number and will go through all pages in postal.

---
+++ navigate.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import time
+import scrape_site
+
+
+driver = webdriver.Chrome()
+
+
+def navigate_site(postalcode):
+    driver.get(f'https://www.boligsiden.dk/salgspris/solgt/villa/1/?postnummer={postalcode}')
+
+    time.sleep(10)  
+
+    click_site(1,postalcode,1)
+
+def click_site(pageelem,postalcode,sitenumber):    
+    driver.get(f'https://www.boligsiden.dk/salgspris/solgt/villa/{sitenumber}/?postnummer={postalcode}')
+    time.sleep(5)
+    try:
+        page = driver.find_element_by_xpath(f'/html/body/div[4]/div/div/div/div[3]/div[1]/div[{pageelem}]/div[2]/div[1]/a')
+        page.click()
+        time.sleep(5)
+        scrape_site.scrape_page(driver)
+        time.sleep(5)
+        click_site(pageelem+1,postalcode,sitenumber)
+    except NoSuchElementException:
+        try:
+            driver.find_element_by_class_name('card__header')  #might be a good idea to find a more unique element on last site page. 
+            print('process complete')
+            return
+        except NoSuchElementException:
+            click_site(1,postalcode,sitenumber+1)
+
+#navigate_site(xxxx)
+
+
+
